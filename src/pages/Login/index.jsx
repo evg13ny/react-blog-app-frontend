f7b9844c4ec774ad664e4c0c8422166b8863c889
Evgeny Polyakov <evg_pol@yahoo.com>
2022-11-26 19:01:50 +0300

    added auth function

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -5,24 +5,54 @@ import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 
 import styles from "./Login.module.scss";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { fetchAuth } from "../../redux/slices/auth";
 
 export const Login = () => {
+  const dispatch = useDispatch()
+
+  const { register, handleSubmit, setError, formState: { errors, isValid } } = useForm({
+    defaultValues: {
+      email: 'test@test.test',
+      password: 'test',
+    },
+    mode: 'onChange',
+  })
+
+  const onSubmit = (values) => {
+    dispatch(fetchAuth(values))
+  }
+
+  console.log(errors, isValid)
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Вход в аккаунт
       </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Неверно указана почта"
-        fullWidth
-      />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Войти
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          type="email"
+          {...register('email', { required: 'Input email' })}
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          label="Пароль"
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          {...register('password', { required: 'Input password' })}
+          fullWidth
+        />
+        <Button type="submit" size="large" variant="contained" fullWidth>
+          Войти
+        </Button>
+      </form>
     </Paper>
   );
 };
